f86d01bb9cc0e47472e239e54ff44fe46d5676c6
Gabriel <nelson.ferrazlemos@heig-vd.ch>
2026-09-22 10:37:59 +0200

    Serie 1

---
+++ Exercices/Serie1.js
@@ -0,0 +1,18 @@
+// Exercice 1 TROUVER LE MAX
+function getMax(a, b, c){
+    return Math.max(a, b, c);
+}
+
+const max = getMax(1,5,8);
+
+console.log(max);
+
+// Exercice 2 NOMBRE ALEATOIRE
+
+function Aleatoire(min, max){
+
+    return Math.random(min, max);
+}
+
+console.log(Aleatoire(1,100));
+
